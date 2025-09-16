bbb7ee9b9e3191793e03dbd614becbfe2a71c335
chestercheng <hi@chester.ch>
2025-09-17 00:14:06 +0800

    feat: add CFP page and submission button
    Signed-off-by: chestercheng <hi@chester.ch>

--- configurations/constants.ts
+++ configurations/constants.ts
@@ -2,4 +2,6 @@ export const COOKIE_LOCALE_KEY = '__sciwork_locale';
 export const DEFAULT_LOCALE = 'en';
 export const UNKNOWN_LOCALE = '';
 export const REGISTER_URL =
-  'https://sciwork.kktix.cc/events/sciwork-conference-20241214-ntu';
+  'https://sciwork.kktix.cc/events/sciwork-conference-20251213-ntu';
+export const CFP_URL =
+  'https://pretalx.sciwork.dev/sw25/submit';
--- configurations/routes.ts
+++ configurations/routes.ts
@@ -1,38 +1,13 @@
 const routes = [
   {
-    path: "/",
-    name: "Welcome",
+    path: "/cfp",
+    name: "Call for Proposals",
     disabled: false,
-  },
-  {
-    path: "/program",
-    name: "Program",
-    disabled: true,
-  },
-  {
-    path: "/training",
-    name: "Training",
-    disabled: true,
-  },
-  {
-    path: "/projects",
-    name: "Projects",
-    disabled: true,
-  },
-  {
-    path: "/venue",
-    name: "Venue",
-    disabled: true,
   },
   {
     path: "/code-of-conduct",
     name: "Code of conduct",
-    disabled: true,
-  },
-  {
-    path: "/financial-aid",
-    name: "Financial Aid",
-    disabled: true,
+    disabled: false,
   },
 ];
 
