9e1a988cf77349e8b8a9ddaa3dfe485d64a31a64
Gene Lin <geneeee0315@gmail.com>
2025-11-26 04:04:38 +0800

    Add sub-menu for workshop

--- configurations/routes.ts
+++ configurations/routes.ts
@@ -8,6 +8,18 @@ const routes = [
     path: "/workshops",
     name: "Workshops",
     disabled: false,
+    children: [
+      {
+        path: "/workshops#jugg-ai",
+        name: "AI 需求工作坊",
+        disabled: false,
+      },
+      {
+        path: "/workshops#steve-gns3",
+        name: "技術工作坊 - GNS3 介紹",
+        disabled: false,
+      },
+    ],
   },
   {
     path: "/sponsor",
