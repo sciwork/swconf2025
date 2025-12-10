6c25fc37e5724c25bcf378f0e4c9093fb3983085
Kuan-Hsien Lee <39944951+KHLee529@users.noreply.github.com>
2025-12-10 19:31:34 +0800

    Add team avatar (#44)
    * Add team avatar
    
    * Update Campus Routing to higher resolution picture

--- configurations/teams.ts
+++ configurations/teams.ts
@@ -1,4 +1,26 @@
 import { StaticImageData } from "next/image";
+import ChesterImg from '@/assets/swconf25_team_avatar/chester.jpg';
+import ChunHsuImg from '@/assets/swconf25_team_avatar/Chun-Hsu.jpg';
+import DboyImg from '@/assets/swconf25_team_avatar/dboy.jpg';
+import LiHungImg from '@/assets/swconf25_team_avatar/lihung.jpg';
+import ChunLinImg from '@/assets/swconf25_team_avatar/Chun-Lin.jpg';
+import KuanHsienImg from '@/assets/swconf25_team_avatar/kuanhsien.jpeg';
+import JieYinImg from '@/assets/swconf25_team_avatar/gene.png';
+import JennyImg from '@/assets/swconf25_team_avatar/Jenny.jpg';
+import YuFangImg from '@/assets/swconf25_team_avatar/yufang.jpg';
+import MaiImg from '@/assets/swconf25_team_avatar/mai.jpeg';
+import WuXianImg from '@/assets/swconf25_team_avatar/5X.png';
+import ChunShihImg from '@/assets/swconf25_team_avatar/ChunShih.png';
+import JunLiImg from '@/assets/swconf25_team_avatar/junli.jpg';
+import PeterWolfImg from '@/assets/swconf25_team_avatar/PeterWolf.png';
+import SteveImg from '@/assets/swconf25_team_avatar/stevechan.png';
+import ShihEnImg from '@/assets/swconf25_team_avatar/ShihEn.png';
+import VeraImg from '@/assets/swconf25_team_avatar/vera.jpeg';
+import YungYuImg from '@/assets/swconf25_team_avatar/YungYu.jpeg';
+import HanXuanImg from '@/assets/swconf25_team_avatar/HanXuan.jpg';
+import RobertImg from '@/assets/swconf25_team_avatar/Robert.png';
+import ZongHanImg from '@/assets/swconf25_team_avatar/Zonghan.jpg';
+import ShuHsiImg from '@/assets/swconf25_team_avatar/ShuHsi.png';
 
 type TeamType = {
   title: string;
@@ -15,82 +37,83 @@ const teams: TeamType[] = [
   {
     title: "Core",
     members: [
-      { name: "Chun-Hsu" },
-      { name: "Kuan-Hsien" },
-      { name: "Yung-Yu" },
+      { name: "Chun-Hsu", image: ChunHsuImg },
+      { name: "Kuan-Hsien", image: KuanHsienImg },
+      { name: "Yung-Yu", image: YungYuImg },
     ],
   },
   {
     title: "Finance & Venue",
     members: [
-      { name: "Chun-Hsu" },
-      { name: "Li-Hung" },
-      { name: "Vera" },
-      { name: "Yung-Yu" },
-      { name: "Robert" },
+      { name: "Chun-Hsu", image: ChunHsuImg },
+      { name: "Li-Hung", image:  LiHungImg },
+      { name: "Vera", image: VeraImg },
+      { name: "Yung-Yu", image: YungYuImg },
+      { name: "Robert", image: RobertImg },
     ],
   },
   {
     title: "Website",
     members: [
-      { name: "Chester" },
-      { name: "Wu-Xian" },
-      { name: "Steve" },
-      { name: "Jie-Yin" },
+      { name: "Chester", image: ChesterImg },
+      { name: "Wu-Xian", image: WuXianImg },
+      { name: "Steve", image: SteveImg },
+      { name: "Jie-Yin", image: JieYinImg },
+      { name: "Chun-Lin", image: ChunLinImg },
     ],
   },
   {
     title: "Registration",
-    members: [{ name: "Jie-Yin" }],
+    members: [{ name: "Jie-Yin", image: JieYinImg }],
   },
   {
     title: "Program",
     members: [
-      { name: "Chun-Shih" },
-      { name: "Han-Xuan" },
+      { name: "Chun-Shih", image: ChunShihImg },
+      { name: "Han-Xuan", image: HanXuanImg },
       { name: "Po-Chi" },
-      { name: "Suci" },
+      { name: "Shu-Hsi", image: ShuHsiImg },
     ],
   },
   {
     title: "Reviewer",
     members: [
-      { name: "Yung-Yu" },
-      { name: "Dboy" },
-      { name: "Kuan-Hsien" },
-      { name: "Chun-Hsu" },
-      { name: "Chester" },
-      { name: "Suci" },
-      { name: "Chun-Shih" },
+      { name: "Yung-Yu", image: YungYuImg },
+      { name: "Shu-Hsi", image: ShuHsiImg },
+      { name: "Zhong-Han", image: ZongHanImg },
+      { name: "Peter", image: PeterWolfImg },
+      { name: "Chester", image: ChesterImg },
+      { name: "Steve", image: SteveImg },
+      { name: "En Shih", image: ShihEnImg },
+      { name: "Chun-Hsu", image: ChunHsuImg },
+      { name: "Dboy", image: DboyImg },
       { name: "Terry" },
-      { name: "Jie-Yin" },
-      { name: "Zhong-Han" },
-      { name: "Steve" },
-      { name: "Peter" },
-      { name: "En Shih" },
+      { name: "Chun-Shih", image: ChunShihImg },
+      { name: "Kuan-Hsien", image: KuanHsienImg },
+      { name: "Jie-Yin", image: JieYinImg },
     ],
   },
   {
     title: "Promotion",
     members: [
-      { name: "Chun-Lin" },
-      { name: "Jenny" },
-      { name: "Zhong-Han" },
-      { name: "Mai Mai" },
-      { name: "Jie-Yin" },
+      { name: "Chun-Lin", image: ChunLinImg },
+      { name: "Jenny", image: JennyImg },
+      { name: "Zhong-Han", image: ZongHanImg },
+      { name: "Mai Mai", image: MaiImg },
+      { name: "Jie-Yin", image: JieYinImg },
     ],
   },
   {
     title: "Venue (Logistics)",
     members: [
-      { name: "Li-Hung" },
+      { name: "Li-Hung", image:  LiHungImg },
       { name: "Terry" },
-      { name: "Yu-Fang" },
+      { name: "Yu-Fang", image: YuFangImg },
       { name: "Hao-Ti" },
       { name: "Kun-Xian" },
-      { name: "Chun-Li" },
+      { name: "Jun-Li", image: JunLiImg },
       { name: "Wei-Syuan" },
-      { name: "En Shih" },
+      { name: "En Shih", image: ShihEnImg },
     ],
   },
 ];
